9742680ea18868c82437197bf8a75fbfaf14a618
Martin Holý <m.holyjr@gmail.com>
2024-11-22 21:54:27 +0100

    Fix layout of team creation page

--- resources/js/Pages/Teams/Create.tsx
+++ resources/js/Pages/Teams/Create.tsx
@@ -1,19 +1,18 @@
 import CreateTeamForm from "@/Pages/Teams/Partials/CreateTeamForm";
 import AppLayout from "@/Layouts/AppLayout";
 import React from "react";
+import { Header } from "@/Components/Header";
 
 export default function Create() {
   return (
     <AppLayout
       title="Create Team"
       renderHeader={() => (
-        <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-          Create Team
-        </h2>
+        <Header title="Create Team" />
       )}
     >
       <div>
-        <div className="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+        <div className="mx-auto py-10 ">
           <CreateTeamForm />
         </div>
       </div>
