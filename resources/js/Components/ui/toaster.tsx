55d7bcbe25054afa56d1511c1d6e32c7bf022bf7
Martin Holý <m.holyjr@gmail.com>
2025-01-10 13:20:26 +0100

    Fix: toaster

--- resources/js/Components/ui/toaster.tsx
+++ resources/js/Components/ui/toaster.tsx
@@ -1,4 +1,4 @@
-import { useToast } from "@/hooks/use-toast"
+import { useToast } from "@/Hooks/use-toast";
 import {
   Toast,
   ToastClose,
@@ -6,10 +6,11 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
+} from "@/Components/ui/toast";
+import React from "react";
 
 export function Toaster() {
-  const { toasts } = useToast()
+  const { toasts } = useToast();
 
   return (
     <ToastProvider>
@@ -25,9 +26,9 @@ export function Toaster() {
             {action}
             <ToastClose />
           </Toast>
-        )
+        );
       })}
       <ToastViewport />
     </ToastProvider>
-  )
+  );
 }
